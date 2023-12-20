e5d5d804cdc931cdb80ee4b2356ef097d871d580
3aa49ec6bfc910647fa1c5a013e48eef <3aa49ec6bfc910647fa1c5a013e48eef@domain.com>
2023-12-20 20:04:01 +0800

    Updated homepage

--- src/website/src/routes/+page.svelte
+++ src/website/src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
 	let a = 0;
 	let b = 0;
 	let total = 0;
-    let logFiles: string[] = [];
+	let logFiles: string[] = [];
+	let finishedApiCalls = false;
 
 	async function add() {
 		const response = await fetch('/api/dashboard', {
@@ -18,39 +19,48 @@
 		total = await response.json();
 	}
 
-    async function listLogFiles() {
-        const response = await fetch('/api/logFiles', {
-            method: 'GET',
-            headers: {
-                'content-type': 'application/json'
-            }
-        });
+	async function listLogFiles() {
+		const response = await fetch('/api/logFiles', {
+			method: 'GET',
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
 
-        const logFiles = await response.json();
-        console.log(logFiles.logFiles);
+		const logFiles = await response.json();
+		console.log(logFiles.logFiles);
 
-        return logFiles.logFiles;
-    }
+		return logFiles.logFiles;
+	}
 
-    onMount(async () => {
-        logFiles = await listLogFiles();
-        console.log(logFiles);
-    });
+	onMount(async () => {
+		logFiles = await listLogFiles();
+		console.log(logFiles);
+		finishedApiCalls = true;
+	});
 </script>
 
 <h1>node-teslausb</h1>
 
 <h2>Log Files</h2>
-<ul>
-    {#each logFiles as logFile}
-        <li><a href={`/viewLog/${logFile}`}>{logFile}</a></li>
-    {/each}
-</ul>
+{#if finishedApiCalls == true}
+	{#if logFiles.length == 0}
+		<p>No log files found.</p>
+	{:else}
+		<ul>
+			{#each logFiles as logFile}
+				<li><a href={`/viewLog/${logFile}`}>{logFile}</a></li>
+			{/each}
+		</ul>
+	{/if}
+{:else}
+	<p>Loading...</p>
+{/if}
 
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <input type="number" bind:value={a}> +
 <input type="number" bind:value={b}> =
 {total}
 
-<button on:click={add}>Calculate</button>
+<button on:click={add}>Calculate</button> -->
