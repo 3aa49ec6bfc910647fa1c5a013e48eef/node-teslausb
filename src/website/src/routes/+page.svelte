bb6506333ab5535d0d1db1485539fd3fba360e66
3aa49ec6bfc910647fa1c5a013e48eef <3aa49ec6bfc910647fa1c5a013e48eef@domain.com>
2023-12-20 08:02:01 +0800

    Added test api

--- src/website/src/routes/+page.svelte
+++ src/website/src/routes/+page.svelte
@@ -1,2 +1,27 @@
+<script>
+	let a = 0;
+	let b = 0;
+	let total = 0;
+
+	async function add() {
+		const response = await fetch('/api/dashboard', {
+			method: 'POST',
+			body: JSON.stringify({ a, b }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		total = await response.json();
+	}
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<input type="number" bind:value={a}> +
+<input type="number" bind:value={b}> =
+{total}
+
+<button on:click={add}>Calculate</button>
