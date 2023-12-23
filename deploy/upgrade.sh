51cf2eb4b462a73aab4fac54953c038876bf73ec
3aa49ec6bfc910647fa1c5a013e48eef <3aa49ec6bfc910647fa1c5a013e48eef@domain.com>
2023-12-23 10:48:13 +0800

    Add upgrade script to fetch and install worker build

--- deploy/upgrade.sh
+++ deploy/upgrade.sh
@@ -6,6 +6,32 @@
 cd /bin/node-teslausb
 git fetch --all
 git reset --hard origin/main
+
+# Get worker build
+USER="3aa49ec6bfc910647fa1c5a013e48eef"
+REPO="node-teslausb"
+FILENAME="worker.zip"
+
+# Fetch the latest release asset URL
+ASSET_URL=$(curl -s https://api.github.com/repos/$USER/$REPO/releases/latest \
+| jq -r ".assets[] | select(.name == \"$FILENAME\") | .browser_download_url")
+
+# Check if the URL is valid
+if [ -z "$ASSET_URL" ]; then
+    echo "Asset URL not found."
+    exit 1
+fi
+
+# Download the file to /tmp/worker.zip
+curl -L $ASSET_URL -o /tmp/worker.zip
+
+# Remove existing worker directory
+sudo rm -rf /bin/node-teslausb/build/worker
+
+# Extract the file to /bin/node-teslausb/build/worker (this is risky to unzip to root, fix later)
+unzip -o /tmp/worker.zip -d /bin/node-teslausb
+
+# Install required modules for worker
 cd /bin/node-teslausb/build/worker && npm i --production
 
 cat << EOF > /lib/systemd/system/node-teslausb.service
